41a2c35ecfbc459c88d97c3fa1f92667a65ecd28
yuji nunome <nunome@kousokuya.co.jp>
2019-05-21 18:11:59 +0900

    ac

--- 20190519/e.cpp
+++ 20190519/e.cpp
@@ -1,52 +1,48 @@
 #include <iostream>
-#include <tuple>
+#include <queue>
 #include <vector>
-
-class Hint {
- public:
-  int y, z;
-  Hint(int _y, int _z) : y(_y), z(_z){};
-};
 
 int main() {
   int N, M;
   std::cin >> N >> M;
-  std::vector<std::vector<Hint>> hint(N);
+  std::vector<std::vector<int>> hint(N);
   std::vector<int> lut(N);
 
   for (int i = 0; i < M; ++i) {
     int x, y, z;
     std::cin >> x >> y >> z;
-    hint[x - 1].push_back(Hint(y - 1, z));
+    hint[x - 1].push_back(y - 1);
+    hint[y - 1].push_back(x - 1);
   }
 
+  std::queue<int> chain;
   int num = 0;
+
   for (int i = 0; i < N; ++i) {
     int table;
     if (lut[i] == 0) {
       ++num;
-      table = num;
+      lut[i] = num;
     } else {
-      table = lut[i];
+      continue;
     }
 
-    for (auto &h : hint[i]) {
-      lut[i] = table;
-      lut[h.y] = table;
-    }
-  }
+    chain.push(i);
 
-  int nohint = 0;
-  for (auto &l : lut) {
-    if (l == 0) {
-      ++nohint;
+    while (!chain.empty()) {
+      auto c = chain.front();
+      for (auto &h : hint[c]) {
+        if (lut[h] == 0) {
+          lut[h] = num;
+          chain.push(h);
+        }
+      }
+      hint[c].clear();
+      chain.pop();
     }
   }
 
   int result = num;
-  if ((num == 1) && (nohint == 0)) {
-    result = 0;
-  }
 
   std::cout << result << std::endl;
 
