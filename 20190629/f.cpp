f6c1ba322cd3d15a2651e20f8ec92f0f2b392fc3
yuji nunome <nunome@kousokuya.co.jp>
2019-07-05 16:46:55 +0900

    modまで追加したけど例3の結果に合わない

--- 20190629/f.cpp
+++ 20190629/f.cpp
@@ -2,8 +2,10 @@
 #include <iostream>
 #include <vector>
 
+const int64_t MOD = 1000 * 1000 * 1000 + 7;
+
 int main() {
-  int n, k;
+  int64_t n, k;
   std::cin >> n >> k;
 
   std::vector<int64_t> cand;
@@ -11,34 +13,39 @@ int main() {
   cand.push_back(0);
   num.push_back(0);
 
-  for (int64_t i = 1, j = 0; i <= n; ++i) {
+  for (int64_t i = 1;;) {
     auto div = n / i;
-    if (div != j) {
-      j = div;
-      cand.push_back(div);
-      num.push_back(1);
-    } else {
-      auto iter = (num.end() - 1);
-      ++(*iter);
+    cand.push_back(div);
+    auto next = n / div + 1;
+    num.push_back(next - i);
+    i = next;
+    if (div == 1) {
+      break;
     }
   }
+  std::cout << cand.size() << std::endl;
 
   --k;
   auto size = cand.size();
   std::vector<int64_t> dp(size * k);
 
-  for (int64_t i = 0; i < k; ++i) {
+  dp[0] = 0;
+  for (int64_t j = 1; j < size; ++j) {
+    int64_t &now = dp[j];
+    const int64_t &left = dp[j - 1];
+    now = ((cand[j] * num[j]) % MOD + left) % MOD;
+  }
+
+  for (int64_t i = 1; i < k; ++i) {
     dp[i * size + 0] = 0;
     for (int64_t j = 1; j < size; ++j) {
-      const int64_t &up = dp[(i - 1) * size + j];
-      const int64_t &left = dp[i * size + j - 1];
-      const int64_t &lu = dp[(i - 1) * size + j - 1];
       int64_t &now = dp[i * size + j];
+      const int64_t &left = dp[i * size + j - 1];
 
-      if (i == 0)
-        now = n / j + left;
-      else
-        now = (dp[j] - dp[j - 1]) * (up - lu) + left;
+      const int64_t &up = dp[(i - 1) * size + j];
+      const int64_t &lu = dp[(i - 1) * size + j - 1];
+      const auto diff = (up + MOD - lu) % MOD;
+      now = ((cand[j] * diff) % MOD + left) % MOD;
     }
   }
 
@@ -46,25 +53,25 @@ int main() {
 
   printf("     |");
   for (int j = 0; j < size; ++j) {
-    printf("%5d", j);
+    printf("%d,", j);
   }
   std::cout << "\n----------------------\n";
   for (int i = 0; i < k; ++i) {
     printf("%5d|", i + 2);
     for (int j = 0; j < size; ++j) {
-      printf("%5d", dp[i * size + j]);
+      printf("%d,", dp[i * size + j]);
     }
     std::cout << std::endl;
   }
 
-  std::cout << "\n----------------------\n";
-  for (int i = 0; i < k; ++i) {
-    printf("%5d|", i + 2);
-    for (int j = 1; j < size; ++j) {
-      printf("%5d", dp[i * size + j] - dp[i * size + j - 1]);
-    }
-    std::cout << std::endl;
-  }
+  // std::cout << "\n----------------------\n";
+  // for (int i = 0; i < k; ++i) {
+  //   printf("%5d|", i + 2);
+  //   for (int j = 1; j < size; ++j) {
+  //     printf("%5d", dp[i * size + j] - dp[i * size + j - 1]);
+  //   }
+  //   std::cout << std::endl;
+  // }
   return 0;
 }
 
