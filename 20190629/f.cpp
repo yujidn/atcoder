5786e5c42cdc751a385237a7db8e43b9447b6451
yuji nunome <nunome@kousokuya.co.jp>
2019-07-05 13:22:08 +0900

    opt

--- 20190629/f.cpp
+++ 20190629/f.cpp
@@ -6,14 +6,30 @@ int main() {
   int n, k;
   std::cin >> n >> k;
 
+  std::vector<int64_t> cand;
+  std::vector<int64_t> num;
+  cand.push_back(0);
+  num.push_back(0);
+
+  for (int64_t i = 1, j = 0; i <= n; ++i) {
+    auto div = n / i;
+    if (div != j) {
+      j = div;
+      cand.push_back(div);
+      num.push_back(1);
+    } else {
+      auto iter = (num.end() - 1);
+      ++(*iter);
+    }
+  }
+
   --k;
-  int size = n + 1;
+  auto size = cand.size();
   std::vector<int64_t> dp(size * k);
 
   for (int64_t i = 0; i < k; ++i) {
     dp[i * size + 0] = 0;
     for (int64_t j = 1; j < size; ++j) {
-      const int64_t inc = dp[j] - dp[j - 1];
       const int64_t &up = dp[(i - 1) * size + j];
       const int64_t &left = dp[i * size + j - 1];
       const int64_t &lu = dp[(i - 1) * size + j - 1];
@@ -22,7 +38,7 @@ int main() {
       if (i == 0)
         now = n / j + left;
       else
-        now = inc * (up - lu) + left;
+        now = (dp[j] - dp[j - 1]) * (up - lu) + left;
     }
   }
 
@@ -41,6 +57,14 @@ int main() {
     std::cout << std::endl;
   }
 
+  std::cout << "\n----------------------\n";
+  for (int i = 0; i < k; ++i) {
+    printf("%5d|", i + 2);
+    for (int j = 1; j < size; ++j) {
+      printf("%5d", dp[i * size + j] - dp[i * size + j - 1]);
+    }
+    std::cout << std::endl;
+  }
   return 0;
 }
 
