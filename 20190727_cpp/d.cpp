46a8f38c50f5cd5e6bd8808818d450ddef35ff42
yuji nunome <yu.ji.d.is+github2@gmail.com>
2019-07-28 00:22:02 +0900

    ac

--- 20190727_cpp/d.cpp
+++ 20190727_cpp/d.cpp
@@ -9,42 +9,48 @@ int main() {
   std::cin >> s;
 
   // i番目の文字を見た時に13との剰余がnになる個数を数え上げる
-  std::vector<int64_t> dp((s.length() + 1) * 13);
+  std::vector<int64_t> dp(s.length() * 13);
 
-  dp[0] = 1;
+  {
+    int val = -1;
+    if (s[0] != '?') val = s[0] - '0';
+    if (val == -1) {
+      for (int i = 0; i < 10; ++i) {
+        dp[i % 13] = 1;
+      }
+    } else {
+      dp[val % 13] = 1;
+    }
+    // % 13は不要だけど定義に帰るため
+  }
 
-  for (int i = 0, e = s.length(); i < e; ++i) {
-    // ?ならvalは-1になる
+  for (int i = 1, e = s.length(); i < e; ++i) {
     int val = -1;
     if (s[i] != '?') val = s[i] - '0';
-
-    // 今がhatenaなら
     if (val == -1) {
-      // 次の文字は0-10のどれかで
-      for (int next = 0; next < 10; ++next) {
-        // 現在地の剰余の結果は0-13のどこかになるので
-        for (int here = 0; here < 13; ++here) {
-          // here * 10 + nextでひっ算的なことができる
-          auto &next_dp = dp[(i + 1) * 13 + ((here * 10 + next) % 13)];
-          const auto &here_dp = dp[i * 13 + here];
-          next_dp = (next_dp + here_dp) % MOD;
+      for (int here = 0; here < 10; ++here) {
+        for (int pre = 0; pre < 13; ++pre) {
+          auto &here_dp = dp[i * 13 + ((pre * 10 + here) % 13)];
+          const auto &pre_dp = dp[(i - 1) * 13 + pre];
+          here_dp = (here_dp + pre_dp) % MOD;
         }
       }
     } else {
-      for (int here = 0; here < 13; ++here) {
-        auto &next_dp = dp[(i + 1) * 13 + ((here * 10 + val) % 13)];
-        const auto &here_dp = dp[i * 13 + here];
-        next_dp = (next_dp + here_dp) % MOD;
+      for (int pre = 0; pre < 13; ++pre) {
+        auto &here_dp = dp[i * 13 + ((pre * 10 + val) % 13)];
+        const auto &pre_dp = dp[(i - 1) * 13 + pre];
+        here_dp = (here_dp + pre_dp) % MOD;
       }
     }
   }
-  std::cout << dp[s.length() * 13 + 5] << std::endl;
 
-  for (int i = 0, e = s.length(); i < e; ++i) {
-    for (int j = 0; j < 13; ++j) {
-      printf("%5d", dp[i * 13 + j]);
-    }
-    std::cout << std::endl;
-  }
+  std::cout << dp[(s.length() - 1) * 13 + 5] << std::endl;
+
+  // for (int i = 0, e = s.length(); i < e; ++i) {
+  // for (int j = 0; j < 13; ++j) {
+  //   printf("%5d", dp[i * 13 + j]);
+  // }
+  // std::cout << std::endl;
+  //}
   return 0;
 }
