5cb7205adc918505490da7d544f7af89451e7c13
yuji nunome <yu.ji.d.is+github2@gmail.com>
2019-08-12 23:44:50 +0900

    コメント追加

--- 20190810_cpp/c.cpp
+++ 20190810_cpp/c.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 
+// ハッシュ計算で使う素数
 constexpr int prime[] = {2,  3,  5,  7,  11, 13,  17,  19, 23, 29,
                          31, 37, 41, 43, 47, 53,  59,  61, 67, 71,
                          73, 79, 83, 89, 97, 101, 103, 107};
@@ -11,19 +12,11 @@ constexpr int prime[] = {2,  3,  5,  7,  11, 13,  17,  19, 23, 29,
 constexpr size_t alphabet = 26;
 constexpr size_t str_len = 10;
 
-inline int64_t hash(std::string &str) {
-  int64_t result = 1;
-
-  for (size_t i = 0; i < str_len; ++i) {
-    result *= prime[str[i] - 'a'];
-  }
-
-  return result;
-}
-
 inline int64_t hash(char *str, int start, int end) {
   int64_t result = 1;
 
+  // アルファベット1文字ごとに素数を割り当てて掛け算結果をハッシュ値にする
+  // オーバーフローしない限りは完全ハッシュ
   for (size_t i = start; i < end; ++i) {
     result *= prime[str[i] - 'a'];
   }
@@ -36,6 +29,7 @@ int main() {
   scanf("%d", &n);
   std::vector<int64_t> sn(n);
 
+  // 最大入力数*1行の文字数(改行込み)
   char input[100000 * 11 + 5];
 
   // 文字列の一括入力
@@ -46,8 +40,11 @@ int main() {
     sn[i] = hash(input, i * 11 + 1, i * 11 + 10 + 1);
   }
 
+  // 文字列ソートではなく、数値ソートになる
+  // unorderd_map使えばソートなくなるけど遅くなる
   std::sort(sn.begin(), sn.end());
 
+  // 最後に合算する
   int64_t count = 0;
   int64_t local_count = 0;
   for (int i = 1; i < n; ++i) {
@@ -58,6 +55,7 @@ int main() {
     }
   }
 
+  // ldで出力しないとWAする
   printf("%ld\n", count);
 
   return 0;
